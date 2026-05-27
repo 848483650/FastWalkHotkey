8924e143531befb521cca680efba66730fc3d222
Maxfield <848483650@qq.com>
2026-05-27 15:23:16 +0800

    添加项目文件。

---
+++ ModEntry.cs
@@ -0,0 +1,27 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastWalkHotkey
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += OnButtonPressed;
+        }
+
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            if (Context.IsWorldReady)
+            {
+                this.Monitor.Log($"{Game1.player.Name} 按下了 {e.Button}.", LogLevel.Debug);
+            }
+        }
+    }
+}
