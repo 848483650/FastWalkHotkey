7d391bffd8dad7895cfbaf3d7a14f5c56db1cc66
Maxfield <848483650@qq.com>
2026-05-27 16:29:11 +0800

    完成翻译与配置设置

---
+++ ModConfig.cs
@@ -0,0 +1,16 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastWalkHotkey
+{
+    public class ModConfig
+    {
+        //88为X键
+        public KeybindList SpeedHotKey { get; set; } = new KeybindList((SButton)88);
+    }
+}
--- ModEntry.cs
+++ ModEntry.cs
@@ -1,26 +1,68 @@
-﻿using StardewModdingAPI;
+﻿using GenericModConfigMenu;
+using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FastWalkHotkey
 {
     public class ModEntry : Mod
     {
+        private ModConfig Config;
+        private bool isToggle = false;
         public override void Entry(IModHelper helper)
         {
+            this.Config = this.Helper.ReadConfig<ModConfig>();
+            helper.Events.GameLoop.GameLaunched += onGameLaunched;
+            helper.Events.GameLoop.SaveLoaded += onSaveLoaded;
             helper.Events.Input.ButtonPressed += OnButtonPressed;
+            helper.Events.Input.ButtonReleased += OnButtonReleased;
+        }
+        private void onGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+                return;
+            configMenu.Register(
+                mod: this.ModManifest,
+                reset: () => this.Config = new ModConfig(),
+                save: () => this.Helper.WriteConfig(this.Config)
+            );
+
+            configMenu.AddKeybindList(
+                mod: this.ModManifest,
+                name: () => this.Helper.Translation.Get("config.keybindFastSpeed.name"),
+                tooltip: () => this.Helper.Translation.Get("config.keybindFastSpeed.desc"),
+                getValue: () => this.Config.SpeedHotKey,
+                setValue: value => this.Config.SpeedHotKey = value
+                );
+        }
+        private void onSaveLoaded(object sender, SaveLoadedEventArgs e)
+        {
+            isToggle = false;
         }
 
         private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
         {
-            if (Context.IsWorldReady)
+            if(!Context.IsWorldReady)
             {
-                this.Monitor.Log($"{Game1.player.Name} 按下了 {e.Button}.", LogLevel.Debug);
+                return;
+            }
+            if(Config.SpeedHotKey.JustPressed() && Game1.activeClickableMenu == null)
+            {
+                this.isToggle = true;
+            }
+            this.Monitor.Log($"{Game1.player.Name} 按下了 {e.Button}.", LogLevel.Debug);
+        }
+        private void OnButtonReleased(object sender, ButtonReleasedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+            {
+                return;
+            }
+            this.Monitor.Log($"{Game1.player.Name} 松开了 {e.Button}.", LogLevel.Debug);
+            if (Config.SpeedHotKey.JustPressed() && Game1.activeClickableMenu == null)
+            {
+                this.isToggle = false;
             }
         }
     }
